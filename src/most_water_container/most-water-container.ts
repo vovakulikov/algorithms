a8e2aad73ff997fd0b3503fd8efc851ab3dceb4a
Vova Kulikov <vovakulikov@icloud.com>
2020-07-24 02:03:30 +0300

    Added description for most water container problem

--- src/most_water_container/most-water-container.ts
+++ src/most_water_container/most-water-container.ts
@@ -1,5 +1,22 @@
 
 
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+// n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+// Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+// For instance
+
+// 8     *
+// 7     * ------ *
+// 6     * ------ *
+// 5     *  * --- *
+// 4     *  * --- *
+// 3     *  *  *  *
+// 2  *  *  *  *  *   *
+// 1  *  *  *  *  *   *
+
+// So the answer for example above is [1, 4] cause max area of water places right between this two lines
+
 export function getMostWaterContainer(field: Array<number>): [number, number] {
   
   if (!Array.isArray(field)) {
